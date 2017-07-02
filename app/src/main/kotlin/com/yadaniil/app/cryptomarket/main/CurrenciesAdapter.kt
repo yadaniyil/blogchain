e0d03392a645ddef4aae5c9d2ab21d8c6b498a1d
Daniil Yakovlev <danny.yakovlev@gmail.com>
2017-07-02 16:19:12 +0300

    Added icons to currencies in list

--- app/src/main/kotlin/com/yadaniil/app/cryptomarket/main/CurrenciesAdapter.kt
+++ app/src/main/kotlin/com/yadaniil/app/cryptomarket/main/CurrenciesAdapter.kt
@@ -1,5 +1,6 @@
 package com.yadaniil.app.cryptomarket.main
 
+import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
@@ -10,15 +11,17 @@ import io.realm.OrderedRealmCollection
 import io.realm.RealmRecyclerViewAdapter
 import org.jetbrains.anko.find
 import android.view.LayoutInflater
-
-
+import android.widget.ImageView
+import com.yadaniil.app.cryptomarket.Application
+import timber.log.Timber
 
 
 /**
  * Created by danielyakovlev on 7/2/17.
  */
 
-class CurrenciesAdapter constructor(data: OrderedRealmCollection<CurrencyRealm>?, autoUpdate: Boolean)
+class CurrenciesAdapter constructor(data: OrderedRealmCollection<CurrencyRealm>?, autoUpdate: Boolean,
+                                    private var context: Context)
     : RealmRecyclerViewAdapter<CurrencyRealm, CurrenciesAdapter.CurrencyViewHolder>(data, autoUpdate) {
 
     init { setHasStableIds(true) }
@@ -35,12 +38,24 @@ class CurrenciesAdapter constructor(data: OrderedRealmCollection<CurrencyRealm>?
             data = currencyRealm
             name.text = currencyRealm?.name
             usdRate.text = currencyRealm?.priceUsd
+            rank.text = currencyRealm?.rank.toString()
+            val resourceId = context.resources?.getIdentifier(
+                    currencyRealm?.symbol.toString().toLowerCase(),
+                    "drawable", context.packageName)
+            try {
+                icon.background = context.resources?.getDrawable(resourceId!!)
+            } catch (e: Exception) {
+                Timber.e(e.message)
+            }
+
         }
     }
 
     class CurrencyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         var name: TextView = view.find<TextView>(R.id.item_currency_name)
         var usdRate: TextView = view.find<TextView>(R.id.item_currency_usd_rate)
+        var icon: ImageView = view.find<ImageView>(R.id.item_currency_icon)
+        var rank: TextView = view.find<TextView>(R.id.item_currency_rank)
         var data: CurrencyRealm? = null
     }
 }

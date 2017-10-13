ecbdbc462093ced821d552a86faee37ac7cfdacb
Daniil Yakovlev <danny.yakovlev@gmail.com>
2017-10-13 16:07:03 +0300

    Disabled not implemented items in Navigation Drawer

--- app/src/main/java/com/yadaniil/blogchain/base/BaseActivity.kt
+++ app/src/main/java/com/yadaniil/blogchain/base/BaseActivity.kt
@@ -26,20 +26,20 @@ abstract class BaseActivity : MvpAppCompatActivity(), IBaseView {
     }
 
     private fun setUpNavigationDrawer() {
-        val item1 = PrimaryDrawerItem().withIdentifier(1)
+        val item1 = PrimaryDrawerItem().withIdentifier(1).withEnabled(false)
                 .withName(R.string.drawer_item_market_info).withIcon(R.drawable.icon_market_info).withSelectable(false)
-        val item2 = PrimaryDrawerItem().withIdentifier(2)
+        val item2 = PrimaryDrawerItem().withIdentifier(2).withEnabled(false)
                 .withName(R.string.drawer_item_converter).withIcon(R.drawable.icon_converter).withSelectable(false)
-        val item3 = PrimaryDrawerItem().withIdentifier(3)
+        val item3 = PrimaryDrawerItem().withIdentifier(3).withEnabled(false)
                 .withName(R.string.drawer_item_portfolio).withIcon(R.drawable.icon_portfolio).withSelectable(false)
-        val item4 = PrimaryDrawerItem().withIdentifier(4)
+        val item4 = PrimaryDrawerItem().withIdentifier(4).withEnabled(false)
                 .withName(R.string.drawer_item_exchanges).withIcon(R.drawable.icon_exchanges).withSelectable(false)
-        val item5 = PrimaryDrawerItem().withIdentifier(5)
+        val item5 = PrimaryDrawerItem().withIdentifier(5).withEnabled(false)
                 .withName(R.string.drawer_item_ico).withIcon(R.drawable.icon_ico).withSelectable(false)
         val item6 = PrimaryDrawerItem().withIdentifier(6)
                 .withName(R.string.drawer_item_mining).withIcon(R.drawable.icon_mining).withSelectable(false)
                 .withOnDrawerItemClickListener { _, _, _-> startActivity<MiningActivity>(); false }
-        val item7 = PrimaryDrawerItem().withIdentifier(7)
+        val item7 = PrimaryDrawerItem().withIdentifier(7).withEnabled(false)
                 .withName(R.string.drawer_item_settings).withIcon(R.drawable.icon_settings).withSelectable(false)
 
         val headerResult = AccountHeaderBuilder()
@@ -50,7 +50,7 @@ abstract class BaseActivity : MvpAppCompatActivity(), IBaseView {
                                 .withName(R.string.add_google_account)
                                 .withIcon(resources.getDrawable(R.drawable.ic_account_circle_black_24dp)))
                 .withOnAccountHeaderListener({ view, profile, currentProfile ->
-                    toast("To add google account activity")
+//                    toast("To add google account activity")
                     true
                 })
                 .build()
